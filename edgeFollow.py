233c6762065be94f06142072f81017ba642ae65a
Spencer <drach.spencer@gmail.com>
2019-12-15 23:57:24 -0700

    forgot to save

--- edgeFollow.py
+++ edgeFollow.py
@@ -1,6 +1,117 @@
+from gpiozero import PWMOutputDevice
+from gpiozero import DigitalOutputDevice
+from time import sleep
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+
+#///////////////// Define Motor Driver GPIO Pins /////////////////
+# Motor A, Left Side GPIO CONSTANTS
+PWM_DRIVE_LEFT =   8	# ENA - H-Bridge enable pin
+FORWARD_LEFT_PIN = 20	# IN1 - Forward Drive
+REVERSE_LEFT_PIN = 21	# IN2 - Reverse Drive
+# Motor B, Right Side GPIO CONSTANTS
+PWM_DRIVE_RIGHT =   9  # ENB - H-Bridge enable pin
+FORWARD_RIGHT_PIN = 5	# IN1 - Forward Drive
+REVERSE_RIGHT_PIN = 6	# IN2 - Reverse Drive
+
+STANDBY_PIN =       27  # High enables moving
+
+# Initialise objects for H-Bridge GPIO PWM pins
+# Set initial duty cycle to 0 and frequency to 1000
+driveLeft = PWMOutputDevice(PWM_DRIVE_LEFT, True, 0, 1000)
+driveRight = PWMOutputDevice(PWM_DRIVE_RIGHT, True, 0, 1000)
+
+# Initialise objects for H-Bridge digital GPIO pins
+forwardLeft = PWMOutputDevice(FORWARD_LEFT_PIN)
+reverseLeft = PWMOutputDevice(REVERSE_LEFT_PIN)
+forwardRight = PWMOutputDevice(FORWARD_RIGHT_PIN)
+reverseRight = PWMOutputDevice(REVERSE_RIGHT_PIN)
+standby = PWMOutputDevice(STANDBY_PIN)
+
+def allStop():
+	forwardLeft.value = False
+	reverseLeft.value = False
+	forwardRight.value = False
+	reverseRight.value = False
+	driveLeft.value = 0
+	driveRight.value = 0
+
+def forwardDrive():
+	print("going forward")
+	forwardLeft.value = True
+	reverseLeft.value = False
+	forwardRight.value = True
+	reverseRight.value = False
+	driveLeft.value = 0.7
+	driveRight.value = 0.7
+
+def reverseDrive():
+	print("going backwards")
+	forwardLeft.value = False
+	reverseLeft.value = True
+	forwardRight.value = False
+	reverseRight.value = True
+	driveLeft.value = 1.0
+	driveRight.value = 1.0
+
+def spinLeft():
+	print("spin left")
+	forwardLeft.value = False
+	reverseLeft.value = True
+	forwardRight.value = True
+	reverseRight.value = False
+	driveLeft.value = 1.0
+	driveRight.value = 1.0
+
+def SpinRight():
+	print("spin right")
+	forwardLeft.value = True
+	reverseLeft.value = False
+	forwardRight.value = False
+	reverseRight.value = True
+	driveLeft.value = 1.0
+	driveRight.value = 1.0
+
+def forwardTurnLeft():
+	print("forward turn left")
+	forwardLeft.value = True
+	reverseLeft.value = False
+	forwardRight.value = True
+	reverseRight.value = False
+	driveLeft.value = 0.2
+	driveRight.value = 0.8
+
+def forwardTurnRight():
+	print("forward turn right")
+	forwardLeft.value = True
+	reverseLeft.value = False
+	forwardRight.value = True
+	reverseRight.value = False
+	driveLeft.value = 0.8
+	driveRight.value = 0.2
+
+def reverseTurnLeft():
+	print("reverse turn left")
+	forwardLeft.value = False
+	reverseLeft.value = True
+	forwardRight.value = False
+	reverseRight.value = True
+	driveLeft.value = 0.2
+	driveRight.value = 0.8
+
+def reverseTurnRight():
+	print("reverse turn right")
+	forwardLeft.value = False
+	reverseLeft.value = True
+	forwardRight.value = False
+	reverseRight.value = True
+	driveLeft.value = 0.8
+	driveRight.value = 0.2
+
+def updatePWM(right, left):
+	driveLeft.value = left;
+	driveRight.value = right;
 
 # set the maximum value that you can threshhold (255 white)
 alpha_slider_max = 255
@@ -25,6 +136,11 @@ trackbar_name = "Threshold"
 cv2.namedWindow(title_window, cv2.WINDOW_NORMAL)
 
 cv2.createTrackbar(trackbar_name, title_window , 0, 	alpha_slider_max, passfunc)
+
+left = 0.5
+right =0.5
+forwardDrive()
+Kp = 1.0
 
 while(True):
 	# read the image in (note) needs to be swapped over to video but
@@ -75,6 +191,9 @@ while(True):
 	total = im_out.size
 	whitePixels = cv2.countNonZero(im_out)
 	ratio = whitePixels/total
+	left += ratio*Kp
+	right += -ratio*kp
+	updatePWM(right, left)
 	print(ratio)
 	#return ratio
 	if cv2.waitKey(1) & 0xFF == ord('q'):
