c315dfed41dd5f19e4a99698de9a26aac78184ef
Spencer <drach.spencer@gmail.com>
2019-12-15 20:17:19 -0700

    removed a bit

--- simpleOpenCV.py
+++ simpleOpenCV.py
@@ -1,7 +1,7 @@
 import cv2
-import matplotlib as mpl
-from matplotlib import pyplot as plt
-import numpy as np
+# import matplotlib as mpl
+# from matplotlib import pyplot as plt
+# import numpy as np
 
 img = cv2.imread('sidewalk.jpg', cv2.IMREAD_GRAYSCALE)
 cv2.imshow('image', img)
